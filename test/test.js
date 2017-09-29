5aca559ab50525998f776499851bba8f9d00370d
AVASSE <avasse62410@gmail.com>
2017-09-29 15:12:40 +0200

    Added advanced code and test (all coordinates) for story1

--- test/test.js
+++ test/test.js
@@ -5,4 +5,16 @@ var LyngkTestCase = TestCase("LyngkTestCase");
 LyngkTestCase.prototype.testStory1 = function(){
     var coordinates = new Lyngk.Coordinates('A',1);
     assertFalse(coordinates.is_valid());
+
+    var coordinates = new Lyngk.Coordinates('B',6);
+    assertFalse(coordinates.is_valid());
+
+    var coordinates = new Lyngk.Coordinates('C',3);
+    assertTrue(coordinates.is_valid());
+
+    var coordinates = new Lyngk.Coordinates('D',2);
+    assertTrue(coordinates.is_valid());
+
+    var coordinates = new Lyngk.Coordinates('G',5);
+    assertTrue(coordinates.is_valid());
 };
