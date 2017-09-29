ad79d569866876bd3bceeaf93a87d34e72afa0c5
AVASSE <avasse62410@gmail.com>
2017-09-29 15:42:16 +0200

    Added test for story 2

--- test/test.js
+++ test/test.js
@@ -18,3 +18,15 @@ LyngkTestCase.prototype.testStory1 = function(){
     var coordinates = new Lyngk.Coordinates('G',5);
     assertTrue(coordinates.is_valid());
 };
+
+LyngkTestCase.prototype.testStory2 = function(){
+    var sum = 0;
+    //For A1 TO I9 increment sum;
+    for(var i = 'A'.charCodeAt(0); i <= 'J'.charCodeAt(0); i++ ){
+        for (var j = 1; j < 10; j++){
+            var coordinates = new Lyngk.Coordinates(String.fromCharCode(i),j);
+            if (coordinates.is_valid()) sum ++;
+        }
+    }
+    assertEquals("Not valid sum of valid coordinates", sum, 43);
+};
