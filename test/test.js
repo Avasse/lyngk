40ec0fe3d7a57d3563ade8c37dc3733fa6ef8af0
AVASSE <avasse62410@gmail.com>
2017-09-29 14:34:54 +0200

    Added code for story 1

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 LyngkTestCase.prototype.testStory1 = function(){
-    var coordinates= new Lyngk.Coordinates('A',1);
+    var coordinates = new Lyngk.Coordinates('A',1);
     assertFalse(coordinates.is_valid());
 };
