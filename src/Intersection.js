c409433229edc652fc1c5a83a74f85fe7eaf4630
AVASSE <avasse62410@gmail.com>
2017-10-17 14:46:15 +0200

    Added Code for Story 7

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,5 +2,9 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function () {
+
+    this.get_State = function () {
+        return Lyngk.State.VACANT;
+    }
 };
