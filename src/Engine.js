9d15d02c0323e2f068c77692ba06050531c9d484
AVASSE <avasse62410@gmail.com>
2017-11-13 18:50:16 +0100

    Added Code for Story22

--- src/Engine.js
+++ src/Engine.js
@@ -162,11 +162,12 @@ Lyngk.Engine = function () {
                 if (this.is_linear_move(startCoordinates, endCoordinates)) {
                     if(this.is_neighbour(startCoordinates, endCoordinates)) {
                         if (this.isnt_maxHeight(interStart, interEnd)) {
-                            if (interEnd.get_height() > interStart.get_height() && interStart.get_height() < 2 ) {
-                                console.log('ERROR: Coin cannot be move on Stack');
+                            if (interStart.get_height() >= interEnd.get_height()) {
+                                return true;
+                            } else {
+                                console.log('ERROR: Cannot move stack on biggest one');
                                 return false;
                             }
-                            else return true;
                         }
                         else {
                             console.log('ERROR: Stack length cannot be > 5');
